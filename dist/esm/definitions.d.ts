e52dcb5e732ea748b5a3e8bc51f9d8914e47e2ad
Darren Gates <darren.gates@wearehathway.com>
2022-01-20 20:56:47 -0800

    update from gitignore

---
+++ dist/esm/definitions.d.ts
@@ -0,0 +1,29 @@
+declare module '@capacitor/core' {
+    interface PluginRegistry {
+        FaceId: FaceIdPlugin;
+    }
+}
+export interface FaceIdPluginIsAvailableResult {
+    /**  Returns available biometric type or 'None' if unavailable */
+    value: 'None' | 'TouchId' | 'FaceId';
+}
+export interface FaceIdPluginAuthOptions {
+    /** String to display authentication reason */
+    reason?: string;
+}
+export interface FaceIdPlugin {
+    /**
+     * check if Face ID or Touch ID is available
+     * @returns  {Promise}
+     * @resolve {value: string}
+     * @rejects PluginResultError
+     */
+    isAvailable(): Promise<FaceIdPluginIsAvailableResult>;
+    /**
+     * Displays the authentication screen
+     * @returns  {Promise}
+     * @resolve void
+     * @rejects PluginResultError
+     */
+    auth(options?: FaceIdPluginAuthOptions): Promise<void>;
+}
